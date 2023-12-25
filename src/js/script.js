5958bcc11b2d8ab2b0ddb40f1aa7095cd3b4441f
Gabriel Messias <gabrielmessiasdsl22081994@gmail.com>
2023-12-25 16:17:45 -0300

    projeto paralelo

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+//Buscando imagem pelo ID:
+function alterarImagem (sourse) {
+    document.getElementById('image').src = sourse;
+}
+
+//Buscando vídoes pelo ID:
+function alterarVideo(sourse) {
+    document.getElementById('videos').src = sourse;
+}
